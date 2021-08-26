b2b5f5597b4d151f1bbc70f4c658814c3cf3c97b
guoliang <guoliang@enovatemotors.com>
2021-08-27 01:20:18 +0800

    sort on remotecmd

--- api/daily.py
+++ api/daily.py
@@ -289,8 +289,15 @@ def getRemoteCmdEvents(vin, Xaxis, dateList):
             logger.warning(f"dirty message 读不懂:{_temp}")
 
     # 2021-08-27 发生过remotecmd的文件里，时间乱序，所以在这里排序一下
-    print(type(respMessageList))
-    print(respMessageList)
+    b=[]
+    for x in respMessageList:
+        b.append(json.dumps(x))
+
+    b.sort()
+
+    respMessageList = []
+    for x in b:
+        respMessageList.append(json.loads(x))
 
     YHeartbeat = assignArray2TimeSlot(Xaxis, respMessageList)
 
