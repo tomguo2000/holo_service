0462c62bae4f4e535d8fe91805e85ee3cca6c164
guoliang <guoliang@enovatemotors.com>
2021-10-22 19:01:49 +0800

    ai

--- api/holoview.py
+++ api/holoview.py
@@ -123,8 +123,8 @@ def holoview_index():
                 endTime = startTime + (86400-1) * 1000
 
             if int(startTime) > 9999999999:     # 传入的是ms，取个整
-                # startTime = int(int(startTime) / 1000) * 1000
-                startTime = int(startTime)
+                startTime = int(int(startTime) / 1000) * 1000
+                # startTime = int(startTime)
             else:
                 startTime = int(startTime) * 1000
 
